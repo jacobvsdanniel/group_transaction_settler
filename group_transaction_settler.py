20458e9722b2bc0ff4d09ceae1f72fb84db532fd
jacobvsdanniel <jacobvsdanniel@gmail.com>
2025-05-22 12:00:31 +0800

    optimize

--- group_transaction_settler.py
+++ group_transaction_settler.py
@@ -1,9 +1,8 @@
 import csv
 import sys
-import copy
+import heapq
 import logging
 import argparse
-from collections import defaultdict
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
@@ -17,25 +16,11 @@ csv.register_dialect(
 )
 
 
-def show_balance(account_to_credit):
-    for account, credit in sorted(account_to_credit.items(), key=lambda nc: (-nc[1], nc[0])):
-        logger.info(f"[credit] {account} net worth ${credit:,g}")
-    return
-
-
-def write_settlements_to_csv_file(settlement_list, settlement_csv_file):
-    with open(settlement_csv_file, "w", encoding="utf8", newline="") as f:
-        writer = csv.writer(f, dialect="csv")
-        header = ["sender", "receiver", "settlement"]
-        writer.writerow(header)
-        for sending_account, receiving_account, settlement_amount in settlement_list:
-            writer.writerow([sending_account, receiving_account, f"{settlement_amount:g}"])
-    return
-
-
 class Settler:
     def __init__(self):
-        self.account_to_credit = defaultdict(lambda: 0)
+        self.account_list = []
+        self.account_to_index = {}
+        self.credit_list = []
         return
 
     def process_transaction(self, account, transaction):
@@ -47,61 +32,90 @@ class Settler:
             # when Bob received money in real life, his account loses credit
             logger.info(f"[transaction] {account} receive ${-transaction:,g}")
 
-        self.account_to_credit[account] += transaction
+        index = self.account_to_index.get(account, None)
+        if index is None:
+            index = len(self.account_list)
+            self.account_list.append(account)
+            self.account_to_index[account] = index
+            self.credit_list.append(transaction)
+        else:
+            self.credit_list[index] += transaction
         return
 
-    def read_transactions(self, transaction_list):
-        for account, transaction in transaction_list:
-            self.process_transaction(account, transaction)
-        return
-
-    def read_transactions_from_csv_file(self, transaction_file):
-        with open(transaction_file, "r", encoding="utf8", newline="") as f:
-            reader = csv.reader(f, dialect="csv")
-            header = next(reader)
-            assert header == ["account", "transaction"]
-            for account, transaction in reader:
-                transaction = float(transaction)
+    def read_transactions(self, transaction_list=None, transaction_file=None):
+        if transaction_list is not None:
+            for account, transaction in transaction_list:
                 self.process_transaction(account, transaction)
-        return
+            return
 
-    def get_settlements(self):
+        if transaction_file is not None:
+            with open(transaction_file, "r", encoding="utf8", newline="") as f:
+                reader = csv.reader(f, dialect="csv")
+                header = next(reader)
+                assert header == ["account", "transaction"]
+                for account, transaction in reader:
+                    transaction = float(transaction)
+                    self.process_transaction(account, transaction)
+            return
+
+    def get_settlements(self, settlement_file=None):
+        # show current account credits
         logger.info("account credits before settlements:")
-        show_balance(self.account_to_credit)
+        for account, credit in sorted(zip(self.account_list, self.credit_list), key=lambda ac: (-ac[1], ac[0])):
+            logger.info(f"[credit] {account} net worth ${credit:,g}")
+
+        # prepare sorted credits and debts
+        positive_credit_heap = []  # min-heap of (-credit, account index)
+        negative_credit_heap = []  # min-heap of (credit, account index)
+        for index, credit in enumerate(self.credit_list):
+            if credit > 0:
+                heapq.heappush(positive_credit_heap, (-credit, index))
+            elif credit < 0:
+                heapq.heappush(negative_credit_heap, (credit, index))
 
         # calculate settlements
-        account_to_credit = copy.deepcopy(self.account_to_credit)
-        settlement_list = []
+        settlement_list = []  # a list of (sending account, receiving account, settlement amount)
         while True:
-            # collect accounts that have positive/negative credits
-            positive_credit_account_list = []
-            negative_credit_account_list = []
-            for account, credit in account_to_credit.items():
-                if credit > 0:
-                    positive_credit_account_list.append((credit, account))
-                elif credit < 0:
-                    negative_credit_account_list.append((-credit, account))
-
-            # no more settlements can be made if there are no positive accounts or no negative accounts
-            if not positive_credit_account_list or not negative_credit_account_list:
-                break
-
             # the account with the most debt should pay the account with the most credit
-            _, max_credit_account = max(positive_credit_account_list)
-            settlement, max_debt_account = max(negative_credit_account_list)
-            settlement_list.append((max_debt_account, max_credit_account, settlement))
-            account_to_credit[max_debt_account] += settlement
-            account_to_credit[max_credit_account] -= settlement
-            logger.info(f"[settlement] {max_debt_account} should pay ${settlement:,g} to {max_credit_account}")
+            try:
+                negated_credit, receiver_account_index = heapq.heappop(positive_credit_heap)
+                negated_debt, sender_account_index = heapq.heappop(negative_credit_heap)
+            except IndexError:
+                # no more settlements can be made if there are no positive accounts or no negative accounts
+                break
+            sender_account = self.account_list[sender_account_index]
+            receiver_account = self.account_list[receiver_account_index]
 
-        logger.info("if these settlements are made, account credits with be:")
-        show_balance(account_to_credit)
+            # the settlement is the sender's debt
+            settlement = -negated_debt
+            settlement_list.append((sender_account, receiver_account, settlement))
+            logger.info(f"[settlement] {sender_account} should pay ${settlement:,g} to {receiver_account}")
 
-        return settlement_list
+            # update receiver account
+            receiver_credit = -negated_credit - settlement
+            if receiver_credit > 0:
+                heapq.heappush(positive_credit_heap, (-receiver_credit, receiver_account_index))
+            elif receiver_credit < 0:
+                heapq.heappush(negative_credit_heap, (receiver_credit, receiver_account_index))
 
-    def get_settlements_and_write_to_csv_file(self, settlement_file):
-        settlement_list = self.get_settlements()
-        write_settlements_to_csv_file(settlement_list, settlement_file)
+        # show future account credits if settlements are made
+        logger.info("if these settlements are made, account credits will be:")
+        future_credit_list = [0] * len(self.credit_list)
+        for negated_credit, account_index in positive_credit_heap:
+            future_credit_list[account_index] = -negated_credit
+        for credit, account_index in negative_credit_heap:
+            future_credit_list[account_index] = credit
+        for account, credit in sorted(zip(self.account_list, future_credit_list), key=lambda ac: (-ac[1], ac[0])):
+            logger.info(f"[credit] {account} net worth ${credit:,g}")
+
+        # write to file
+        if settlement_file is not None:
+            with open(settlement_file, "w", encoding="utf8", newline="") as f:
+                writer = csv.writer(f, dialect="csv")
+                header = ["sender", "receiver", "settlement"]
+                writer.writerow(header)
+                for sending_account, receiving_account, settlement_amount in settlement_list:
+                    writer.writerow([sending_account, receiving_account, f"{settlement_amount:g}"])
         return settlement_list
 
 
@@ -115,8 +129,8 @@ def main():
             logger.info(f"[arg.{key}] {value}")
 
     settler = Settler()
-    settler.read_transactions_from_csv_file(arg.transaction_file)
-    settler.get_settlements_and_write_to_csv_file(arg.settlement_file)
+    settler.read_transactions(transaction_file=arg.transaction_file)
+    settler.get_settlements(settlement_file=arg.settlement_file)
     return
 
 
